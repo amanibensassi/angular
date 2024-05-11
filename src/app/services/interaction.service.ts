e117b194bfe8eabd8a4849d39d56a67fe884837e
amanibensassi <123634459+amanibensassi@users.noreply.github.com>
2024-05-11 13:32:46 +0200

    Update interaction.service.ts

--- src/app/services/interaction.service.ts
+++ src/app/services/interaction.service.ts
@@ -7,7 +7,7 @@ import { Observable, tap } from 'rxjs';
   providedIn: 'root',
 })
 export class InteractionService {
-  readonly API_URL = 'http://localhost:8089/espritgather/interaction';
+  readonly API_URL = 'http://sql-service:8089/espritgather/interaction';
 
   constructor(private httpClient: HttpClient) {}
 
