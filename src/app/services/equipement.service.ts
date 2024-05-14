00fb2808c3ba80ffa6dadb091374637a74570ee8
amanibensassi <123634459+amanibensassi@users.noreply.github.com>
2024-05-14 14:52:55 +0200

    Update equipement.service.ts

--- src/app/services/equipement.service.ts
+++ src/app/services/equipement.service.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class EquipementService {
-  readonly API_URL = 'http://52.238.248.18:8089/espritgather/equipement';
+  readonly API_URL = '/espritgather/equipement';
 
   constructor(private router: Router, private httpClient: HttpClient) { }
   getAllEquipement() {
